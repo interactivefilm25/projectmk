18ccaf2a6030622afd4152bc947ce8d80f07dd7e
Brent Marshall <info@brentmarshall.tech>
2025-07-24 22:49:23 -0400

    Working on getting the model reporting correct results.

--- webserver/app.py
+++ webserver/app.py
@@ -43,66 +43,39 @@ async def audio_stream():
 
     while True:
         data = await websocket.receive()
-        print(f"Received initial data: {data}")
+        # print(f"Received initial data: {data}")
         
         if data == "PING":
             continue
 
         elif data == "END":
             print("Recording ended")
-    
-            full_audio_array = np.concatenate(audio_data, axis=0)
-            print(f"Full audio array shape: {full_audio_array.shape}")
 
-            if full_audio_array.ndim > 1:
-                signal = full_audio_array[:, 0]  # Use first channel if stereo
-            else:
-                signal = full_audio_array
+            all_bytes = b''.join(audio_data)
+            audio_stream = BytesIO(all_bytes)
+            audio_segment = AudioSegment.from_file(audio_stream, format="webm")
+            audio_segment.export("uploads/streamed_audio.wav", format="wav")
+            print(f"Saved concatenated audio to uploads/streamed_audio.wav")
 
-            features: pd.DataFrame = smile.process_signal(signal, 16000)
-            print(f"Extracted features shape: {features.shape}")
-            print(f"Extracted features: {features.head()}")
-
-            trained_cols = model.feature_names_in_
-            aligned_features = features.reindex(columns=trained_cols, fill_value=0.0)
-
-            probs = model.predict_proba(aligned_features)[0]
-            emo = model.classes_[probs.argmax()]
-            probababilities = dict(zip(model.classes_, (probs * 100).round(2)))
-
-            print(f"Model emotion prediction: {emo}")
-            print(f"Probabilities: {probababilities}")
-
-            await websocket.send(json.dumps(emo).encode())
-            await websocket.send(json.dumps(probababilities).encode())
-
-            audio_data = []
+            await predict()
 
         elif data == "KILL":
             os._exit(0)
 
         else: # Process audio data
-            with open(audio_file_path, "wb") as audio_file:
-                audio_file.write(data)
-                print(f"Received data chunk of size {len(data)} bytes")
+            print(f"Received data chunk of size {len(data)} bytes")
+            audio_data.append(data)
 
-                try:
-                    audio_stream = BytesIO(data)
-                    audio_segment = AudioSegment.from_file(audio_stream, format="webm")
-                    wav_data = BytesIO()
-                    audio_segment.export(wav_data, format="wav")
-                    wav_data.seek(0)
+async def predict():
+    features = smile.process_file("uploads/streamed_audio.wav")
+    trained_cols = model.feature_names_in_
+    aligned_features = features.reindex(columns=trained_cols, fill_value=0.0)
+    probs = model.predict_proba(aligned_features)[0]
+    emo = model.classes_[np.argmax(probs)]
+    probababilities = dict(zip(model.classes_, (probs * 100).round(2)))
 
-                    audio_chunk, _ = sf.read(wav_data, dtype='float32')
-                    audio_data.append(audio_chunk)
-                    print(f"Processed Audio chunk shape: {audio_chunk.shape}")
-                except Exception as e:
-                    print(f"Error processing audio chunk: {e}")
-            
-            print(f"Audio saved to {audio_file_path}")
+    print(f"Model emotion prediction: {emo}")
+    print(f"Probabilities: {probababilities}")
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-    print("Web server running on http://localhost:5000")
-    print("Press Ctrl+C to stop the server")
-    print("Make sure to run the client script to test the audio streaming functionality")
+    await websocket.send(json.dumps(emo).encode())
+    await websocket.send(json.dumps(probababilities).encode())
